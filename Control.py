e25087c5c72548f7cb6719aad2ef2980ae32b794
CS114.K21.KHTN <62535444+ducvuuit@users.noreply.github.com>
2020-07-23 10:31:47 +0700

    first_commit

---
+++ Control.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env pybricks-micropython
+from pybricks.hubs import EV3Brick
+from pybricks.ev3devices import (Motor, TouchSensor, ColorSensor,
+                                 InfraredSensor, UltrasonicSensor, GyroSensor)
+from pybricks.parameters import Port, Stop, Direction, Button, Color
+from pybricks.tools import wait, StopWatch, DataLog
+from pybricks.robotics import DriveBase
+from pybricks.media.ev3dev import SoundFile, ImageFile
+
+class LineFollowing:
+    def __init__(self, left_motor, right_motor,  threshold, proportion, wheel_diameter=55.5, axle_track=104):
+        self.left_motor = left_motor
+        self.right_motor = right_motor
+        self.wheel_diameter = wheel_diameter
+        self.axle_track = axle_track
+        self.threshold = threshold
+        self.proportion = proportion
+        self.robot = DriveBase(left_motor,right_motor,wheel_diameter, axle_track)
+    
+    def run(self, line_sensor, drive_speed):
+        directions = []
+        another_sensor = ColorSensor(Port.S1)
+        while (True):
+            current = line_sensor.reflection()
+            deviation = current - self.threshold
+            turn_rate = deviation * self.proportion
+            self.robot.drive(drive_speed, turn_rate)
+            if (current < 11):
+                directions.append('R')
+                if (another_sensor.reflection() < 20):
+                    directions.append('L')
+                break
+        self.robot.stop()
+        return directions
---
+++ main.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env pybricks-micropython
+from pybricks.hubs import EV3Brick
+from pybricks.ev3devices import (Motor, TouchSensor, ColorSensor,
+                                 InfraredSensor, UltrasonicSensor, GyroSensor)
+from pybricks.parameters import Port, Stop, Direction, Button, Color
+from pybricks.tools import wait, StopWatch, DataLog
+from pybricks.robotics import DriveBase
+from pybricks.media.ev3dev import SoundFile, ImageFile
+from LineFollowing import LineFollowing
+
+# This program requires LEGO EV3 MicroPython v2.0 or higher.
+# Click "Open user guide" on the EV3 extension tab for more information.
+
+
+# Create your objects here.
+ev3 = EV3Brick()
+
+
+# Write your program here.
+#ev3.speaker.beep()
+left_motor = Motor(Port.B)
+right_motor = Motor(Port.C)
+
+line_sensor = ColorSensor(Port.S3)
+linefollow = LineFollowing(left_motor,right_motor,threshold=37, proportion=-1.2)
+list_direction = linefollow.run(line_sensor, drive_speed=20)
+print(list_direction)
